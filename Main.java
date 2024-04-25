51f8e246b6779586a145120f2a73135e2d63cd99
Preston <138165657+Preston-Bohnsack@users.noreply.github.com>
2024-04-25 10:28:02 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,97 +1,110 @@
 import java.util.*;
 
 public class Main{
+  static Scanner in = new Scanner(System.in);
 
   public static void main(String[] args){
-    simWalk(100000, 500, 5, 4, 10);
-    /*
-      Number of random walks made. Increase this to increase confidence. : 100,000 walks
-      How many steps each random walk takes. : 500 steps
-      How much each step will increase if going up. : 5 point(s)
-      How much each step will decrease if going down. Should be positive. : 4 point(s)
-      How high each random walk starts. : 10 point(s)
-    */
+    int walks = 0;
+    int steps = 0;
+    int gain = 5;
+    int loss = 4;
+    int startingHeight = 20;
+    
+    System.out.println("How many walks do you want to simulate? Please enter a positive integer. Try 100 walks.");
+    walks = in.nextInt();
+    in.nextLine();
+    System.out.println();
+    System.out.println("How many steps do you want to each walk to take? Please enter a positive integer. Try 10000 steps.");
+    steps = in.nextInt();
+    in.nextLine();
+    System.out.println();
+    System.out.println("How many points do you want each gain to be? Please enter a positive integer. Try a gain of 5.");
+    gain = in.nextInt();
+    in.nextLine();
+    System.out.println();
+    System.out.println("How many points do you want each loss to be? Please enter a positive integer. Try a loss of 4.");
+    loss = in.nextInt();
+    in.nextLine();
+    System.out.println();
+    System.out.println("How many points do you want each walk to start at? Please enter a positive integer. Try a starting height of 20.");
+    startingHeight = in.nextInt();
+    in.nextLine();
+    System.out.println();
+    
+    simWalk(walks, steps, gain, loss, startingHeight);
   }
 
-  public static void simWalk(int numwalks, int steps, double upHeight, double downHeight, double startingHeight){
-    double[][] walks = manyWalks(numwalks, steps, upHeight, downHeight, startingHeight);
-    walks = floor(walks); // makes sure each walk cannot continue if their points are <= 0
-    System.out.println(numBustWalks(walks) + " out of " + walks.length + " walks were a bust");
-    double avgROI = 0;
-    for(double[] walk : walks){
-      avgROI += walk[walk.length - 1];
+  public static void simWalk(int numwalks, int steps, int gain, int loss, int startingHeight){
+    int[][] walks = manyWalks(numwalks, steps, gain, loss, startingHeight);
+    
+    double n = 0;
+    for(int[] walk : walks){
+      n += walk[walk.length - 1];
     }
-    avgROI /= numwalks;
-    avgROI = (avgROI - startingHeight) / startingHeight;
-    System.out.println("Average ROI: " + avgROI);
+    n /= walks.length;
+    System.out.println("Average Ending Height: " + n);
   }
 
-  public static double[][] manyWalks(int numWalks, int steps, double upHeight, double downHeight, double startingHeight){
-    double[][] walks = new double[numWalks][steps];
+  public static int[][] manyWalks(int numWalks, int steps, int gain, int loss, int startingHeight){
+    int[][] walks = new int[numWalks][steps];
     for(int walk = 0; walk < numWalks; walk++){
-      walks[walk] = walk(steps, upHeight, downHeight, upWeight, downWeight, startingHeight);
+      walks[walk] = walk(steps, gain, loss, startingHeight);
     }
+    
     return walks;
   }
   
-  public static double[] walk(int steps, double upHeight, double downHeight, double startingHeight){
-    double[] walk = new double[steps + 1];
+  public static int[] walk(int steps, int gain, int loss, int startingHeight){
+    int[] walk = new int[steps + 1];
     walk[0] = startingHeight;
     for(int step = 1; step <= steps; step++){
       walk[step] = walk[step - 1] +
-  	((Math.random() < 0.5) ? -downHeight : upHeight);
+        ((Math.random() < 0.5) ? -loss : gain);
     }
-    // printWalk(walk);
     
     return walk;
   }
 
-  public static void printWalk(double[] walk){
-    for(double d : walk){
-      System.out.print(d + " ");
-    }
-    System.out.println();
-    System.out.println();
-  }
-
-  public static int numBustWalks(double[][] walks){
+  public static int numBustWalks(int[][] walks){
     int num = 0;
-    for(double[] walk : walks){
+    for(int[] walk : walks){
       if(bust(walk)){
         num++;
       }
     }
-	  
+    
     return num;
   }
 	
-  public static boolean bust(double[] walk){
-    for(double d : walk){
-      if(d <= 0){
-	return true;
+  public static boolean bust(int[] walk){
+    for(int n : walk){
+      if(n <= 0){
+        return true;
       }
     }
     
     return false;
   }
   
-  public static double[][] floor(double[][] walks){
-    for(double[] walk : walks){
+  public static int[][] floor(int[][] walks){
+    for(int[] walk : walks){
       if(bust(walk)){
         boolean busted = false;
-	  for(int i = 0; i < walk.length; i++){
-	    if(busted){
-	      walk[i] = 0;
-	    }
-            else if(walk[i] <= 0){
-              bust = true;
-              walk[i] = 0;
-	    }
-	  }
+        for(int i = 0; i < walk.length; i++){
+          if(busted){
+            walk[i] = 0;
+          }
+          else if(walk[i] <= 0){
+            busted = true;
+            walk[i] = 0;
+          }
         }
       }
-	  
-      return walks;
     }
-	
+    
+    return walks;
+  }
+  
+  
 }
+
